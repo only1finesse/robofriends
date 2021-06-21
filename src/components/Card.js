49022e79e8ae4350ead325df36cb9c228f90904b
only1finesse <72461240+only1finesse@users.noreply.github.com>
2021-06-21 07:36:41 -0700

    Complete starter files for robofriends app

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+
+const Card = ({ name, email, id }) => {
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow '>
+            <img src={`https://robohash.org/${id}?100x100}`} alt='test pic' />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card; 
---
+++ src/containers/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+
+export class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobot = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+
+
+        if (!this.state.robots.length) {
+            return (
+                <div className='tc'>
+                    <h1 className='f1' >RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <h1>Loading...</h1>)
+                    </Scroll>
+                </div>
+            )
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1' >RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobot} />
+                    </Scroll>
+                </div>
+            )
+        }
+    }
+}
+
+export default App;
